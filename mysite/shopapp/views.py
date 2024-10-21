71a20ba5bdc11610ff1857938aa73404badae621
kikolas <mr.nifasse@mail.ru>
2024-10-21 19:17:33 +0300

    + Django Progect - 3

---
+++ mysite/shopapp/views.py
@@ -0,0 +1,17 @@
+from itertools import product
+from  timeit import default_timer
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+def shop_index(request: HttpRequest):
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999)
+    ]
+
+    context = {
+        "time_running": default_timer(),
+        "products": products
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
