cdeaf4b067b2f6b95ed9011e54e21bbbeb49a223
kikolas <mr.nifasse@mail.ru>
2024-12-10 20:14:28 +0300

    8.8

--- mysite/shopapp/forms.py
+++ mysite/shopapp/forms.py
@@ -1,14 +1,20 @@
-from django import forms
+from django.contrib.auth.models import Group
+from django.forms import ModelForm
 from django.core import validators
 from .models import Product, Order
 
-class ProductForm(forms.ModelForm):
+class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = "name", "price", "description", "discount"
 
-class OrderForm(forms.ModelForm):
+class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = "deliveri_address", "promocode", "user", "products"
+
+class GroupForm(ModelForm):
+    class Meta:
+        model = Group
+        fields = ["name"]
 
--- mysite/shopapp/urls.py
+++ mysite/shopapp/urls.py
@@ -1,14 +1,34 @@
 from django.urls import path
-from .views import shop_index, groups_list, products_list, orders_list, create_product,create_orders
+from .views import (
+    ShopIndexView,
+    GroupsListView,
+    ProductDetailsView,
+    ProductsListView,
+    OrdersListViev,
+    OrdersDetailViev,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
+    OrderUpdateView,
+    OrderCreateView,
+    OrderDeleteView,
+)
 
 app_name = "shopapp"
 
 urlpatterns = [
-    path("", shop_index, name='index'),
-    path("groups/", groups_list, name='groups_list'),
-    path("products/", products_list, name='products_list'),
-    path("products/create/", create_product, name='product_create'),
-    path("orders/", orders_list, name='orders_list'),
-    path("orders/create", create_orders, name='create_order'),
+    path("", ShopIndexView.as_view(), name='index'),
+    path("groups/", GroupsListView.as_view(), name='groups_list'),
+    path("products/", ProductsListView.as_view(), name='products_list'),
+    path("products/create/", ProductCreateView.as_view(), name='product_create'),
+    path("products/<int:pk>/", ProductDetailsView.as_view(), name='product_details'),
+    path("products/<int:pk>/update/", ProductUpdateView.as_view(), name='product_update'),
+    path("products/<int:pk>/archive/", ProductDeleteView.as_view(), name='product_archive'),
+    path("orders/", OrdersListViev.as_view(), name='orders_list'),
+    path("orders/create/", OrderCreateView.as_view(), name='create_order'),
+    path("orders/<int:pk>/", OrdersDetailViev.as_view(), name='order_details'),
+    path("orders/<int:pk>/update", OrderUpdateView.as_view(), name='order_update'),
+    path("orders/<int:pk>/confirm_delete/", OrderDeleteView.as_view(), name='order_delete'),
+
 
 ]
